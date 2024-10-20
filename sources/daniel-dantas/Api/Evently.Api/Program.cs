dd23dbbe6fbac53b75dea813f684901e13b644f9
danieldantasdev <contatodanieldantasdev@gmail.com>
2024-10-19 23:36:28 -0300

    chore: remove lines code for instances controllers in a Program.cs

--- sources/daniel-dantas/Api/Evently.Api/Program.cs
+++ sources/daniel-dantas/Api/Evently.Api/Program.cs
@@ -2,7 +2,6 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,7 +18,5 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
